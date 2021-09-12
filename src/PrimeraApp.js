c91414b0ee7a34b190d73713b94c753256f182ad
sefhirot69 <josalillo@gmail.com>
2021-09-12 17:49:23 +0200

    feat : comunicacion entre componentes

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, {Fragment} from "react";
+// import React, {Fragment} from "react";
 
-const PrimeraApp = () => {
-    return <h1>Hola Mundo</h1>;
+
+/**
+ * !importante se puede poner un compoente entre la
+ * etiqueta  <Fragment></Fragment> o con la etiquetas cortas <></>
+ */
+
+const PrimeraApp = ({saludo = 'Hola mundo'} ) => {
+
+    console.log(saludo);
+
+    return (
+        <>
+            <h1>{saludo}</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
 }
 
 export default PrimeraApp;
