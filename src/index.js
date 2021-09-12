1032960067ab6377157bd31403beb7b21efd1730
sefhirot69 <josalillo@gmail.com>
2021-09-12 18:31:51 +0200

    feat : tarea CounterApp.js de https://gist.github.com/Klerith/e1a731cc595c00a9794a709062eae757

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from 'react-dom';
-import PrimeraApp from "./PrimeraApp";
+import CounterApp from './CounterApp';
+// import PrimeraApp from './PrimeraApp';
 import './index.css';
 
 const divRoot = document.querySelector('#app');
 
-ReactDOM.render(<PrimeraApp saludo={'Hola mundo'}/>, divRoot);
+ReactDOM.render(<CounterApp value={1}/>, divRoot);
