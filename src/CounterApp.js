733ed96dd1db1432d4e7c88199efbc904a3e52a5
sefhirot69 <josalillo@gmail.com>
2021-09-13 10:06:19 +0200

    feat : eventos

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ({value}) => {
+const CounterApp = ( { value } ) => {
+
+    // handleAdd
+    const handleAdd = (e) => {
+        console.log(e);
+    };
+
     return (
         <>
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2> { value } </h2>
+
+            <button onClick={ handleAdd }>+1</button>
         </>
     );
 };
