d17dc3202fef0e7472a684acdb30cb725827bc76
sefhirot69 <josalillo@gmail.com>
2021-09-13 10:20:51 +0200

    feat : hooks - useSate

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,25 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 
-const CounterApp = ( { value } ) => {
+const CounterApp = () => {
+
+    const [counter, setCounter] = useState(0); // [] retorna un array
 
     // handleAdd
-    const handleAdd = (e) => {
-        console.log(e);
+    const handleAdd = () => {
+        setCounter(counter + 1);
+        // setCounter( (c) => c + 1  );
     };
 
     return (
         <>
             <h1>CounterApp</h1>
-            <h2> { value } </h2>
+            <h2> {counter} </h2>
 
-            <button onClick={ handleAdd }>+1</button>
+            <button onClick={handleAdd}>+1</button>
         </>
     );
 };
 
 CounterApp.propTypes = {
-    value : PropTypes.number
+    value: PropTypes.number
 };
 
 export default CounterApp;
