7a95a77e5ae9e5f39f42e9dfa5ffba9c215e8430
sefhirot69 <josalillo@gmail.com>
2021-09-14 13:44:44 +0200

    feat : tests unitarios sobre los boton incrementar/decrementar

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -3,12 +3,16 @@ import CounterApp from "../CounterApp";
 
 describe('Pruebas sobre CounterApp', () => {
 
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<CounterApp/>);
+    });
+
     test('Debe de mostrar <CounterApp/> correctamente', () => {
 
         const h1 = 'CounterApp';
         const h2 = '10';
-
-        const wrapper = shallow(<CounterApp/>);
 
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('h1').text()).toBe(h1);
@@ -22,5 +26,26 @@ describe('Pruebas sobre CounterApp', () => {
         const wrapper = shallow(<CounterApp value={100}/>);
 
         expect(wrapper.find('h2').text().trim()).toBe(h2);
+    });
+
+    test('Debe de incrementar con el boton de +1', () => {
+
+        const btn1 = wrapper.find('button').at(0).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect( counterText ).toBe('11');
+
+    });
+
+
+    test('Debe de decrementar con el boton de -1', () => {
+
+        wrapper.find('button').at(2).simulate('click');
+
+        const counterText = wrapper.find('h2').text().trim();
+
+        expect( counterText ).toBe('9');
+
     });
 });
