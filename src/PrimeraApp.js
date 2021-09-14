56fe48ceaa2dc6f30052d206e94f66a795633e1a
sefhirot69 <josalillo@gmail.com>
2021-09-14 12:02:43 +0200

    feat : instalo enzyme-to-json. Cambio logica de tests en PrimeraApp.js y ignoro los snapshots generados

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -14,7 +14,7 @@ const PrimeraApp = ({saludo, subtitulo}) => {
 
     return (
         <>
-            <h1>{saludo}</h1>
+            <h1>{saludo}!!!</h1>
             <p>{subtitulo}</p>
         </>
     );
--- src/tests/PrimeraApp.test.js
+++ src/tests/PrimeraApp.test.js
@@ -1,15 +1,22 @@
 import {render} from "@testing-library/react";
 import PrimeraApp from "../PrimeraApp";
+import {shallow} from "enzyme";
 
 describe('Pruebas en <PrimeraApp />', () => {
 
-    test('debe de mostrar el mensaje "Hola soy Goku"', () => {
+    // test('debe de mostrar el mensaje "Hola soy Goku"', () => {
+    //
+    //     const saludo = 'Hola soy Goku';
+    //
+    //     const { getByText } = render(<PrimeraApp saludo={saludo} />);
+    //
+    //     expect(getByText(saludo)).toBeInTheDocument()
+    // });
+    test('debe de mostrar <PrimeraApp /> correctamente', () => {
 
         const saludo = 'Hola soy Goku';
+        const wrapper = shallow( <PrimeraApp saludo={saludo}/>);
 
-        const { getByText } = render(<PrimeraApp saludo={saludo} />);
-
-        expect(getByText(saludo)).toBeInTheDocument()
+        expect(wrapper).toMatchSnapshot();
     });
-
 });
